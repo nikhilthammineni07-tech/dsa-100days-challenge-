3cf973db5d1279273f683b2bbabbaa786f033dcd
nikhilthammineni07-tech <nikhilthammineni07@gmail.com>
2026-02-09 16:36:07 +0530

    Add files via upload

---
+++ day9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    int i, j;
+    char temp;
+
+    scanf("%s", s);
+
+    i = 0;
+    j = strlen(s) - 1;
+
+    while (i < j) {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+
+    printf("%s", s);
+    return 0;
+}
